1ec1aa0b04955c71a6a836843a093a72b470409a
EliasSchaut <eschaut@outlook.de>
2023-08-07 18:33:18 +0200

    client: implement page header with table

--- client/components/layout/header.vue
+++ client/components/layout/header.vue
@@ -1,13 +1,53 @@
 <template>
-  <div
-    class="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between"
-  >
-    <h3 class="text-base font-semibold leading-6 text-gray-900">
-      Karaoke List
-    </h3>
-    <div class="mt-3 flex sm:ml-4 sm:mt-0">
-      <SettingLang class="inline-flex items-center px-3 py-2" />
-      <SettingTheme class="inline-flex items-center px-3 py-2" />
+  <div class="border-b border-gray-200 p-5 pb-5">
+    <div class="flex items-center justify-between">
+      <h1 class="text-xl font-semibold leading-6 text-gray-900">
+        Karaoke List
+      </h1>
+      <form
+        class="relative hidden flex-1 sm:mx-4 sm:flex"
+        action="#"
+        method="GET"
+      >
+        <label for="search-field" class="sr-only">Search</label>
+        <MagnifyingGlassIcon
+          class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+          aria-hidden="true"
+        />
+        <input
+          id="search-field"
+          class="block h-full w-full border-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0"
+          placeholder="Search..."
+          type="search"
+          name="search"
+        />
+      </form>
+      <div class="flex items-center justify-center sm:mt-0">
+        <SettingLang class="pr-4" />
+        <SettingTheme />
+      </div>
     </div>
+    <form
+      class="relative mt-4 flex flex-1 sm:mx-4 sm:hidden"
+      action="#"
+      method="GET"
+    >
+      <label for="search-field" class="sr-only">Search</label>
+      <MagnifyingGlassIcon
+        class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+        aria-hidden="true"
+      />
+      <input
+        id="search-field"
+        class="block h-full w-full border-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0"
+        placeholder="Search..."
+        type="search"
+        name="search"
+      />
+    </form>
   </div>
 </template>
+
+<script setup lang="ts">
+import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid';
+</script>
---
+++ client/pages/[...slug].vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="px-8 lg:px-8">
+    <div class="-mx-4 mt-8 sm:-mx-0">
+      <table class="min-w-full divide-y divide-gray-300">
+        <thead>
+          <tr>
+            <th
+              scope="col"
+              class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+            >
+              Info
+            </th>
+            <th
+              scope="col"
+              class="table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+            >
+              Title
+            </th>
+            <th
+              scope="col"
+              class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+            >
+              Artist
+            </th>
+            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+              <span class="sr-only">Edit</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-200 bg-white">
+          <tr v-for="track in tracks" :key="track.title">
+            <td
+              class="max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0"
+            >
+              Info
+            </td>
+            <td
+              class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0"
+            >
+              {{ track.title }}
+              <dl class="font-normal sm:hidden">
+                <dt class="sr-only">Artist</dt>
+                <dd class="mt-1 truncate text-gray-700">{{ track.artist }}</dd>
+              </dl>
+            </td>
+            <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+              {{ track.artist }}
+            </td>
+            <td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+              <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                Report
+                <span class="sr-only">Edit</span>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Index',
+  data() {
+    return {
+      tracks: [
+        {
+          title: 'In the end',
+          artist: 'Linking Park',
+        },
+        {
+          title: 'Burn It Down',
+          artist: 'Linking Park',
+        },
+        {
+          title: 'Castle Of Glass',
+          artist: 'Linking Park',
+        },
+      ],
+    };
+  },
+});
+</script>
